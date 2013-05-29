64e0f39d83f49d307e1f180656714836a8749c15
Justin Gordon <justin.gordon@gmail.com>
2013-05-28 16:39:55 -1000

    run: rails g controller static index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 App::Application.routes.draw do
+  get "static/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
