aed5cfa0b06f77d9f1d23a4ac595552d8c9cfd25
Justin Gordon <justin.gordon@gmail.com>
2013-06-11 10:52:58 -1000

    Updated production.rb so rake task for db:populate can access model by setting config.dependency_loading = true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,9 @@ App::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   config.ember.variant = :production
+
+  # Enable access to model from rake tasks
+  # http://guides.rubyonrails.org/configuring.html
+  #config.threadsafe! # not set as default for Rails4
+  config.dependency_loading = true if $rails_rake_task
 end
